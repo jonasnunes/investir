280d3df6e80abe257726a22f29506499689a06fa
jonasnunes <jonas.nunees@yahoo.com.br>
2022-06-03 09:36:14 -0300

    update project
    iniciando projeto
    header concluído

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Investir</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet"> 
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <div id="corpo">
+        <header>
+            <img src="images/dolar.png" width="45" alt="imagem">
+            <h2>Investir</h2>
+        </header>
+    </div>
+</body>
+</html>
