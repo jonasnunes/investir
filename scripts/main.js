d45056aaad92ff993bec7fb477edd36928b4ff62
jonasnunes <jonas.nunees@yahoo.com.br>
2022-06-03 22:02:55 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,12 @@
 function simular(){
-    var valor = document.getElementById('valor')
-    var tempo = document.getElementById('tempo')
+    var valor = document.getElementById('valor').value
+    var tempo = document.getElementById('tempo').value
 
     var meses = tempo * 12
-    var juros = valor * 0.01
-    var total = 0
-
-    for(var i = tempo; i <= meses; i++){
-        total = total + valor
-        juros = total * 0.01
-        total = total + juros
-    }
+    var taxaJuros = 1 + (12 / 100)
+    var valorTotal = valor * meses
+    var total = valorTotal * (taxaJuros ** tempo)
     
-    alert('Valor: ' + valor.value + 'Tempo: ' + tempo.value + total.value)
+    document.getElementById('resultado').innerHTML = 'Se você guardar R$ ' + valor + ' todo mês, durante ' + meses + ' meses, no final você terá R$ ' + total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
 }
 
